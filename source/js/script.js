e8fc69cf55249426549993d7f9ebda983a2b6756
Svetlana <krapiva1910@gmail.com>
2022-02-01 16:50:26 +0300

    обновление

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,31 @@
-var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
-  navMain.classList.remove('main-nav--nojs');
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--opened')) {
+    navMain.classList.remove('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--opened');
+  }
+});
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+function modalShow(className) {
+  const modal = document.querySelector('.modal');
+  const orders = document.querySelectorAll(className);
+
+  orders.forEach(function(item) {
+    item.addEventListener('click', function(evt) {
+      evt.preventDefault();
+      modal.classList.add('modal--active');
+    });
+  })
+
+  document.addEventListener('keydown', function(evt) {
+    if (evt.key === "Escape") {
+      modal.classList.remove('modal--active');
     }
   });
+}
+
+modalShow('.product__button');
+modalShow('.card__button');
